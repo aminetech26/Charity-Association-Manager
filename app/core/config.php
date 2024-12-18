1e72bec1a1e5f29bb581d50a81c57082bc7c1e2e
aminetech26 <la_guerraiche@esi.dz>
2024-12-18 13:52:32 +0100

    config mvc architecture

---
+++ app/core/config.php
@@ -0,0 +1,13 @@
+<?php 
+    /** database config **/
+	define('DBNAME', 'my_db');
+	define('DBHOST', 'localhost');
+	define('DBUSER', 'root');
+	define('DBPASS', '');
+	define('DBDRIVER', '');
+	define('ROOT', 'http://localhost/TDWProject/public');
+
+    define('APP_NAME', "My TDWProject");
+    define('APP_DESC', "");
+
+    define('DEBUG', true);
