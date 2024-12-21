440e0f39b5100ac13b81d32c25e9780c2ed82687
aminetech26 <la_guerraiche@esi.dz>
2024-12-21 20:21:38 +0100

    feat: add methods to retrieve members with and without subscriptions

--- app/models/Membre.model.php
+++ app/models/Membre.model.php
@@ -6,8 +6,20 @@ class MembreModel{
     protected $table = "Compte_Membre";
     protected $allowedColumns = ['nom', 'prenom', 'email', 'mot_de_passe', 'photo', 'piece_identite', 'adresse', 'numero_de_telephone', 'abonnement_id'];
 
+    public function getAllMembers(){
+        return $this->findAll();
+    }
 
+    public function getMemberById($id){
+        return $this->where(['id' => $id]);
+    }
 
+    public function getMembersWithSubscription(){
+        return $this->query("SELECT * FROM Compte_Membre WHERE abonnement_id IS NOT NULL");
+    }
 
+    public function getMembersWithoutSubscription(){
+        return $this->query("SELECT * FROM Compte_Membre WHERE abonnement_id IS NULL");
+    }
 
 }
